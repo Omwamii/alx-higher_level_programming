c453b07a9d3d3ab0e94379c209c21f3d5cc4da1e
omwami <ianalpha@protonmail.com>
2023-03-07 22:34:33 +0300

    Technical interview insert sorted list problem

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -20,7 +20,7 @@ listint_t *insert_node(listint_t **head, int number)
 
 	if (*head == NULL || (*head)->n > number)
 	{
-		new->next = (*head)->next;
+		new->next = *head;
 		*head = new;
 		return (new);
 	}
