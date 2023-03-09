306426d548f228534bf926d361dad899f73a2dc3
omwami <ianalpha@protonmail.com>
2023-03-09 23:35:02 +0300

    prints names in hidden_4 module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     """Print all names defined in hidden_4 module"""
     import hidden_4
-    
+
     data = sorted(dir(hidden_4))
     for n in data:
         if n[:2] != "__":
