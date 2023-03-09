53f379ae81277624eeafe0d55c4034f3bc9e6bbe
omwami <ianalpha@protonmail.com>
2023-03-09 23:33:45 +0300

    prints names in hidde_4 module

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Print all names defined in hidden_4 module"""
+    import hidden_4
+    
+    data = sorted(dir(hidden_4))
+    for n in data:
+        if n[:2] != "__":
+            print(n)
