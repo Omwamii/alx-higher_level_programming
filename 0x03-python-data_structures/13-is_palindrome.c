dda3e744177e39c29d9190ff0d8deccfcb514cfe
omwami <ianalpha@protonmail.com>
2023-03-14 23:13:03 +0300

    Technical interview problem 'check for palindrome'

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,73 @@
+#include "lists.h"
+#include <string.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare_int(int a[], int b[], size_t size);
+/**
+ * is_palindrome - checks if single list has palindrome
+ * @head: pointer to pointer to first node
+ *
+ * Return: 0 if not, 1 if it is
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *current = *head;
+	int *fwd, *rev;
+	size_t i = 0;
+	size_t size = sizeof(int);
+	void *wd;
+	int j, num;
+
+	if (current == NULL) /* empty list */
+		return (1);
+
+
+	fwd = malloc(size);
+	while (current->next != NULL)
+	{
+		size += sizeof(int);
+		fwd[i++] = (current->n);
+		current = current->next;
+		wd = realloc(fwd, size);
+		if (!wd)
+			return (-1);
+	}
+	fwd[i] = current->n;
+	j = i;
+	i = 0;
+	rev = malloc(size);
+	for (; j >= 0; j--, i++)
+		rev[i] = fwd[j];
+	for (i = 0; i < (size / sizeof(int)); i++)
+		printf("%d ", fwd[i]);
+	printf("\n");
+	for (i = 0; i < (size / sizeof(int)); i++)
+		printf("%d ", rev[i]);
+	printf("\n");
+	num = compare_int(fwd, rev, (size / sizeof(int)));
+	free(fwd), free(rev);
+	return (num);
+}
+
+/**
+ * compare_int - compares integer to check equality
+ * @a: first array
+ * @b: reversed array
+ * @size: array size
+ *
+ * Return: 0 if not equal, else 1
+ */
+int compare_int(int *a, int *b, size_t size)
+{
+	size_t i = 0;
+
+	for (; i < size; i++)
+	{
+		if (a[i] != b[i])
+			return (0);
+	}
+
+	return (1);
+}
