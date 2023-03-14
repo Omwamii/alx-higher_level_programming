dffe41fbb07e288ce142fd05e07fd75a38d2f940
omwami <ianalpha@protonmail.com>
2023-03-14 18:07:13 +0300

    return largest int in list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    '''finds biggest integer in list'''
+    if len(my_list) == 0:
+        return None
+
+    biggest = 0
+    for n in my_list:
+        if n > biggest:
+            biggest = n
+    return biggest
