4190d1ee0a99dbbdad1355787fe27d84bee2c1f8
omwami <ianalpha@protonmail.com>
2023-03-15 14:56:54 +0300

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     '''replaces all occurrences of an element by another in a new list.'''
+    if my_list is None or len(my_list) == 0:
+        return my_list
     new_list = my_list.copy()
     index = new_list.index(search)
 
@@ -11,5 +13,4 @@ def search_replace(my_list, search, replace):
             index = new_list.index(search)
         except ValueError:
             break
-
     return new_list
