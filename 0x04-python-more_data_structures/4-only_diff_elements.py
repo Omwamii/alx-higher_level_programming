d09e14eb6174097fd126351aac415d2c934cfde8
omwami <ianalpha@protonmail.com>
2023-03-15 14:50:15 +0300

    prints difference of two sets

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,5 +2,5 @@
 def only_diff_elements(set_1, set_2):
     '''returns set of elements present in only one set'''
     diff = list(set_1.difference(set_2))
-    diff += list(set_2.difference(set_1))    
+    diff += list(set_2.difference(set_1))
     return sorted(diff)
