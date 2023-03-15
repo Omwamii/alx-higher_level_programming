b186ca5f50325e7d2f6223379931439d6060d417
omwami <ianalpha@protonmail.com>
2023-03-15 15:15:12 +0300

    find and replace

--- 0x04-python-more_data_structures/1-main.py
+++ 0x04-python-more_data_structures/1-main.py
@@ -2,9 +2,9 @@
 search_replace = __import__('1-search_replace').search_replace
 
 my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-new = None
+new = [2,2,2,2,2,2,2,2]
 new_list = search_replace(new, 2, 89)
 
-print(new_list)
-print(my_list)
+print(new_list) # new_list
+print(new) # my_list
 
--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     '''replaces all occurrences of an element by another in a new list.'''
-    if my_list is None or len(my_list) == 0:
+    if my_list is None:
         return None
-    new_list = my_list.copy()
-    index = new_list.index(search)
 
-    while index:
+    new_list = my_list.copy()
+    if len(new_list) == 0:
+        return new_list
+    try:
+        index = new_list.index(search)
+    except ValueError:
+        return new_list # no instance found
+
+    while index >= 0:
         new_list.remove(search)
         new_list.insert(index, replace)
         try:
