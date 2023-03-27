705105aef58dade78ec95a47b77254d2ec0b35bf
omwami <ianalpha@protonmail.com>
2023-03-28 00:36:06 +0300

    prints integer

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_print_integer_err(value):
     ''' prints an integer '''
+
     try:
         print("{:d}".format(value))
     except ValueError as val:
