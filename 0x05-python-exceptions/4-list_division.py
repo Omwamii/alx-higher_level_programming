7ade3d337d791f90bef04a291a560f05b206a72b
omwami <ianalpha@protonmail.com>
2023-03-28 00:16:08 +0300

    list division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -17,7 +17,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             result = 0
             print("out of range")
-            return new_list
         finally:
             new_list.append(result)
             i += 1
