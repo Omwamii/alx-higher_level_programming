1ff8f15d5bd8b2f0c625ab8f37e6cd511b35022e
omwami <ianalpha@protonmail.com>
2023-03-29 16:49:05 +0300

    define value validation for 'size'

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" module with square class definition """
+
+
+class Square:
+    """ square class definition """
+    def __init__(self, size=0):
+        """initialize size"""
+        try:
+            size.isdigit()
+        except AttributeError:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
+        else:
+            raise TypeError('size must be an integer')
