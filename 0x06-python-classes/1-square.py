e99ba369d484c80ca3a72ec6101e7d0d2b76a5d9
omwami <ianalpha@protonmail.com>
2023-03-29 16:21:26 +0300

    set private attribute 'size' on square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" module with class square with defined attributes"""
+class Square:
+    """ defines square object"""
+    def __init__(self, size):
+        """ set size parameter """
+        self.__size = size
