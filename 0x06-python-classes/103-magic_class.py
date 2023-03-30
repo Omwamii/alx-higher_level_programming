b7d0aed2e4d3bbe9f7cbaed5189dbb6a73048c05
omwami <ianalpha@protonmail.com>
2023-03-30 17:20:24 +0300

    magicclass disassembled class

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -5,7 +5,7 @@ import math
 
 class MagicClass:
     """ define class """
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         """ initialize class """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
