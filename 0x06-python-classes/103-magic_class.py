e4378b3b8207956eb4db6a8416877f2b2b6d4900
omwami <ianalpha@protonmail.com>
2023-03-30 17:18:24 +0300

    magicclass disassembled class

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -4,15 +4,18 @@ import math
 
 
 class MagicClass:
-
+    """ define class """
     def __init__(self, radius):
+        """ initialize class """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):
+        """ get area """
         return 2 ** self.__radius * math.pi
 
     def circumference(self):
+        """ get circumference """
         return 2 * math.pi * self.__radius
