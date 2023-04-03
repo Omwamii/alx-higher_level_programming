c00f7884dc5e7be57e99f0d1cf68f3d6d338047e
omwami <ianalpha@protonmail.com>
2023-04-03 11:57:07 +0300

    find area , perimeter

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -47,6 +47,4 @@ class Rectangle:
 
     def perimeter(self):
         """ get rect perimeter """
-        if self.__width == 0 or self.__height == 0:
-            return 0
         return 2 * (self.__width + self.__height)
