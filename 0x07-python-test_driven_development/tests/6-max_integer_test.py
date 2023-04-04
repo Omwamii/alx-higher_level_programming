8330244514eb38e1ef638309047a7f7c849e9d4d
omwami <ianalpha@protonmail.com>
2023-04-05 00:15:39 +0300

    unittest module test: 'max_integer'

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Unittest for max_integer([..])
+"""
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """ test class """
+    def setUp(self):
+        """ setup variables """
+        self.list1 = [1, 3, 23, 54]
+        self.list2 = [2.23, 3.43, 0, 0.43]
+        self.list3 = []  # return None for empty list
+        self.list4 = [6]
+        self.list5 = [6, 3.4, 10, 32.5]
+
+    def test_integers_list(self):
+        """ test for max_integer int int list"""
+        ans = max_integer(self.list1)
+        self.assertEqual(ans, 54)
+
+    def test_floats_list(self):
+        """ test list with floats only"""
+        ans = max_integer(self.list2)
+        self.assertEqual(ans, 3.43)
+
+    def test_empty_list(self):
+        """ test empty list"""
+        ans = max_integer(self.list3)
+        self.assertEqual(ans, None)
+
+    def test_single_element(self):
+        """ test list with one element"""
+        ans = max_integer(self.list4)
+        self.assertEqual(ans, 6)
+
+    def test_floats_ints(self):
+        """ test list with floats and ints"""
+        ans = max_integer(self.list5)
+        self.assertEqual(ans, 32.5)
+
+
+if __name__ == "__main__":
+    unittest.main()
