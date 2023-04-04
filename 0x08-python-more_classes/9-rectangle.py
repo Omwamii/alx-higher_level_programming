5d1fc6c21caf0f73613bbaa525061fb7f5eac3eb
omwami <ianalpha@protonmail.com>
2023-04-04 17:07:30 +0300

    return new instance with classmethod

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -10,9 +10,8 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """ initialize variables"""
         type(self).number_of_instances += 1
-        self.height = height
         self.width = width
-
+        self.height = height
     def __del__(self):
         """ define destructor """
         type(self).number_of_instances -= 1
