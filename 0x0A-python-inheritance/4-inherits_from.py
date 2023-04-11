86dfb900a3afa45859240d9fb745d28e487b8185
omwami <ianalpha@protonmail.com>
2023-04-11 20:55:39 +0300

    check for inheritance only

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,6 +6,6 @@ module with 'inherits_from'
 
 def inherits_from(obj, a_class):
     """ return true if obj inherits from a_class"""
-    if issubclass(type(obj), a_class):
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
