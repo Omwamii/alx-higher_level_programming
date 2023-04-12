c53b806f109f7ececea5b8515aef104b7e64e491
omwami <ianalpha@protonmail.com>
2023-04-12 22:48:21 +0300

    write to fil

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+module with 'write_file'
+"""
+
+
+def write_file(filename="", text=""):
+    """ write to a file """
+    with open(filename, mode="w", encoding="utf-8") as f:
+        f.write(text)
