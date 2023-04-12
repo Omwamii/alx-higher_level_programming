0cdce6ec2f34729e58825de9675240ed3e95a155
omwami <ianalpha@protonmail.com>
2023-04-12 22:49:13 +0300

    write to file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,4 +7,4 @@ module with 'write_file'
 def write_file(filename="", text=""):
     """ write to a file """
     with open(filename, mode="w", encoding="utf-8") as f:
-        f.write(text)
+       return  f.write(text)
