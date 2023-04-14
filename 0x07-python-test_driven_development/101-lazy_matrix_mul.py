10b77837a6e1c8d129a3ba969a55af4e16cbb285
omwami <ianalpha@protonmail.com>
2023-04-14 13:58:03 +0300

    lazy matrix

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-module with 'lazy_matrix_mul'
+module with lazy matrix multiplication
 """
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """ multiply matrix using numpy """
-    return np.dot(m_a, m_b)
+    """ multiply matrices with numpy"""
+    return np.matmul(m_a, m_b)
