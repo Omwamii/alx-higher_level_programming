7a25d1e08c148b9c5557e34e8c5293d5f74f2680
omwami <ianalpha@protonmail.com>
2023-04-17 22:16:44 +0300

    add getters & setters for 'size'

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,6 +11,17 @@ class Square(Rectangle):
         """ init vars """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """ size getter """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ size setter """
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """ override __str__ """
         return "[Square] ({}) {}/{} - {}".format(
