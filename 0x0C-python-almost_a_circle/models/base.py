e79f09c5909a88bfea01eb9f8859f9ad514ddb5f
omwami <ianalpha@protonmail.com>
2023-04-18 23:24:03 +0300

    fix 'to_json_stirng'

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,7 +19,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ returns JSON string rep of arg """
-        if list_dictionaries is None:
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
 
