11d8df79de4fb5adf4aff8d2acdc2ee94cd68520
omwami <ianalpha@protonmail.com>
2023-05-29 22:34:08 +0300

    convert database to utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,10 @@
--- script to convert hbtn_0c_0 database to UTF8 in server
-ALTER DATABASE hbtn_0c_0
-CHARACTER SET utf8 COLLATE utf8_general_ci;
+-- script to convert hbtn_0c_0 database, first_table and field 'name'  to UTF8 in server
+
+-- convert database to utf8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+-- convert table to utf8
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- convert field 'name' to utf8
+ALTER TABLE first_table MODIFY COLUMN `name` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_general_ci;
