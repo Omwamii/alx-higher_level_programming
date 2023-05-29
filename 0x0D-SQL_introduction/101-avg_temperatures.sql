36ad96428c8be46152a2fcfca86a633b965f5618
omwami <ianalpha@protonmail.com>
2023-05-29 23:08:50 +0300

    top 3 cities by temp

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,7 +1,4 @@
 -- script that imports a table from another file and displays avg temp
--- import the database
--- SOURCE temperatures.sql
-
 -- display values
 SELECT `city`, AVG(`value`) AS avg_temp
 FROM `temperatures`
