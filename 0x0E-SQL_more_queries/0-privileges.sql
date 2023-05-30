490505a9465bf00d4e2a96878dfcbf3e2b3e0ece
omwami <ianalpha@protonmail.com>
2023-05-30 16:35:43 +0300

    show privileges grantred

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- script to list all privileges of MYSQL users user_0d_1 and user_0d_2
+-- show privileges granted
 SHOW GRANTS FOR `user_0d_1`@`localhost`;
 SHOW GRANTS FOR `user_0d_2`@`localhost`;
