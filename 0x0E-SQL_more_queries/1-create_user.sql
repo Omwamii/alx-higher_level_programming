20a1920cc034e2f6d2895d39cfec9041ec8d8547
omwami <ianalpha@protonmail.com>
2023-05-30 17:07:51 +0300

    create user if not exists

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- script to create a use user_0d_1 on server 
 -- create user
 CREATE USER IF NOT EXISTS'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_pwd';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
