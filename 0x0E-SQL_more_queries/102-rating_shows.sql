7db42f8d26070f88372b260eb9f39911b7630de1
omwami <ianalpha@protonmail.com>
2023-06-02 22:57:09 +0300

    Rotten tomatoes

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,5 @@
 -- script that lists all shows fro `hntn_0d_tvshows_rate` by rating
-SELECT title, SUM(tv_show_ratings.rate) AS 'rating'
+SELECT title, SUM(tv_show_ratings.rate) 'rating'
 FROM tv_shows
 LEFT JOIN tv_show_ratings 
 ON tv_shows_ratings.show_id = tv_shows.id
