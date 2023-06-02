5025248e57e3e472adf619b3ef0724b2ecd9d19b
omwami <ianalpha@protonmail.com>
2023-06-02 23:04:34 +0300

    Rotten tomatoes

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -2,6 +2,6 @@
 SELECT title, SUM(tv_show_ratings.rate) 'rating'
 FROM tv_shows
 LEFT JOIN tv_show_ratings 
-ON tv_shows_ratings.show_id = tv_shows.id
+ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY title
 ORDER BY rating DESC;
