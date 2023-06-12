5a4fd3f455d1a334dc42a167a5c313c8eb9f08f1
omwami <ianalpha@protonmail.com>
2023-06-12 16:02:07 +0300

    find second largest integer

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+if (process.argv.length < 4) {
+  console.log('0');
+} else {
+  const myNums = [];
+  for (let i = 2; i < process.argv.length; i++) {
+    myNums.push(process.argv[i]);
+  }
+  let largest = myNums[0];
+  for (let i = 0; i < myNums.length; i++) {
+    if (myNums[i] > largest) { largest = myNums[i]; }
+  }
+  const index = myNums.indexOf(largest);
+  myNums.splice(index, 1);
+
+  largest = myNums[0];
+  for (let i = 0; i < myNums.length; i++) {
+    if (myNums[i] > largest) { largest = myNums[i]; }
+  }
+  console.log(largest);
+}
