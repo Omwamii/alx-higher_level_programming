249acd821b490cbbc8475fe715ea244eb02214db
omwami <ianalpha@protonmail.com>
2023-06-13 21:08:58 +0300

    concat two files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,7 +2,8 @@
 const fs = require('fs');
 const file1 = process.argv[2];
 const file2 = process.argv[3];
+const file3 = process.argv[4];
 const data1 = fs.readFileSync(file1, 'utf-8');
 const data2 = fs.readFileSync(file2, 'utf-8');
-const concated = (data1 + data2).trim();
-console.log(concated);
+const concated = (data1 + data2);
+fs.writeFileSync(file3, concated);
