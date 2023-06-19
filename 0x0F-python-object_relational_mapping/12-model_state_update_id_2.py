de8cac8ab6efa676155b2c48e572fb8c1a9b80f6
omwami <ianalpha@protonmail.com>
2023-06-19 14:47:16 +0300

    script to change name of State obj

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,46 +1,19 @@
 #!/usr/bin/python3
-"""module to change name of State object
+""" Change name of State obj from the database
 """
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
-# Pass the MySQL username, password, and database name as command-line args
-u_name = sys.argv[1]
-passw = sys.argv[2]
-db_name = sys.argv[3]
 
-# Create the connection string
-connection_string = f'mysql+mysqldb://{u_name}:{passw}@localhost/{db_name}'
-
-# Connect to the MySQL server using the connection string
-engine = create_engine(connection_string)
-
-# Create a session factory
-Session = sessionmaker(bind=engine)
-
-if __name__ == '__main__':
-    # Import State and Base from model_state
-    from model_state import State, Base
-
-    # Create all tables defined by the classes inheriting from Base
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
-
-    # Create a session
+    Session = sessionmaker(bind=engine)
     session = Session()
-
-    # Get the State object with id = 2
-    state = session.query(State).filter(State.id == 2).first()
-
-    if state:
-        # Change the name of the State to "New Mexico"
-        state.name = "New Mexico"
-
-        # Commit the session to persist the changes to the database
-        session.commit()
-    else:
-        print("State not found")
-
-    # Close the session
+    new_instance = session.query(State).filter_by(id=2).first()
+    new_instance.name = 'New Mexico'
+    session.commit()
     session.close()
