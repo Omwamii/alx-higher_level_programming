27a387d50e9aae505460b531694000c9e5c5d9ca
omwami <ianalpha@protonmail.com>
2023-06-19 10:45:22 +0300

    delete states with a

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -7,28 +7,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-# Pass the MySQL username, password, and database name as command-line args
-u_name = sys.argv[1]
-passw = sys.argv[2]
-db_name = sys.argv[3]
 
-# Create the connection string
-conn_string = f'mysql+mysqldb://{u_name}:{passw}@localhost/{db_name}'
+engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                       .format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
-# Connect to the MySQL server using the connection string
-engine = create_engine(conn_string)
-
-# Create a session factory
 Session = sessionmaker(bind=engine)
 
 if __name__ == '__main__':
-    # Import State and Base from model_state
-    from model_state import State, Base
-
-    # Create all tables defined by the classes inheriting from Base
     Base.metadata.create_all(engine)
-
-    # Create a session
     session = Session()
 
     # Query and delete all State objects with a name containing the letter "a"
@@ -38,6 +24,4 @@ if __name__ == '__main__':
 
     # Commit the session to persist the deletions to the database
     session.commit()
-
-    # Close the session
     session.close()
