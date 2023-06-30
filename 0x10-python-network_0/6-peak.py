740e7705658e4742d228592d57837c872aa87031
omwami <ianalpha@protonmail.com>
2023-06-30 13:59:09 +0300

    Technical interview: find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" module to find a peak in a list
+"""
+
+
+def find_peak(arg_list):
+    """ fn to find a peak in a list like: /\.
+    """
+    if not isinstance(arg_list, list):
+        return None
+    if len(arg_list) == 0 or len(arg_list) == 1:
+        return None  # if only one item, no peak formed
+    peak = None
+    for index, item in enumerate(arg_list):
+        if index == 0:  # skip first item as peak
+            continue
+        if peak is None:
+            peak = item
+        else:
+            if peak == item and arg_list[index - 1] == item:
+                if peak < arg_list[0]:
+                    peak = arg_list[0]
+                    break
+                else:
+                    peak = item
+                    break
+            elif peak < item:
+                peak = item
+                if index < len(arg_list) - 1:
+                    if arg_list[index + 1] < item:
+                        break
+    return peak
