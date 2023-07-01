8d502b55efe86146613f318a5e22817cc886bef1
omwami <ianalpha@protonmail.com>
2023-07-01 12:31:06 +0300

    curl options

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script to display all HTTP methods accepted by server
-curl -s -I -X OPTIONS "$1" | grep -i "^Allow:" | cut -d " " -f 2
+curl -s -I -X OPTIONS "$1" | grep -i "^Allow:" | cut -d " " -f2-
