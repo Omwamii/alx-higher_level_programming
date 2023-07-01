475855ad78d9b5d35765ee97c17997a3d5111d8f
omwami <ianalpha@protonmail.com>
2023-07-01 14:17:42 +0300

    catch me if you can

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-# script to make request to server to get 'You got me'
-curl -s -X PUT -L -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+# script to make request to server to get 'You got me!'
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
