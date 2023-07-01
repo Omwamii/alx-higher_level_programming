15558d5dfae27cb86ff90ff0ac1e18fd77ebf849
omwami <ianalpha@protonmail.com>
2023-07-01 13:55:58 +0300

    Technical interview: find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,32 +6,21 @@
 def find_peak(arg_list=None):
     """ fn to find a peak in a list
     """
-    if not isinstance(arg_list, list):
-        return None
     if len(arg_list) == 0 or len(arg_list) == 1:
         return None  # if only one item, no peak formed
     peak = None
     for index, item in enumerate(arg_list):
-        if not isinstance(item, int) and not isinstance(item, float):
-            return None  # not a digit
-        if index == 0:  # skip first item as peak
-            continue
-        if peak is None:
-            peak = item
-        else:
-            if peak == item and arg_list[index - 1] == item:
-                if peak < arg_list[0]:
-                    peak = arg_list[0]
-                    break
-                else:
-                    peak = item
-                    break
-            elif peak < item:
+        if index == 0:
+            if item > arg_list[index + 1]:
                 peak = item
-                if index < len(arg_list) - 1:
-                    if arg_list[index + 1] < item:
-                        break
-    if peak == arg_list[1]:
-        if arg_list[0] > peak:
-            peak = arg_list[0]
+            else:
+                peak = arg_list[index + 1]
+        elif index == len(arg_list) - 1:
+            if item > peak:
+                peak = item  # last point is highest
+        else:
+            if item > arg_list[index - 1] and item > arg_list[index + 1]:
+                return item  # peak found
+            if item == arg_list[index - 1]:
+                break  # plain detected __
     return peak
