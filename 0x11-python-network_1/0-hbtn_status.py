410b1d60d59b0630d0e9cf229d11da5de1d89810
omwami <ianalpha@protonmail.com>
2023-07-04 10:39:52 +0300

    my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ script that fetches https://alx-intranet.hbtn.io/status
 """
+
+
 if "__name__" == "__main__":
     from urllib.request import urlopen
 
@@ -9,6 +11,6 @@ if "__name__" == "__main__":
         body = response.read()
         utf8_content = body.decode('utf-8')
         print('Body response:')
-        print(f'    - type: {type(body)}')
-        print(f'    - content: {body}')
-        print(f'    - utf8 content: {utf8_content}')
+        print(f'\t- type: {type(body)}')
+        print(f'\t- content: {body}')
+        print(f'\t- utf8 content: {utf8_content}')
