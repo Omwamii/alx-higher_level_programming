4a915a12290a808a81c0aeecae94d25d8d176984
omwami <ianalpha@protonmail.com>
2023-07-04 14:13:07 +0300

    what's my status --1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status."""
+"""Script fetches https://alx-intranet.hbtn.io/status"""
 import requests
 
 
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    req_url = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- type: {}".format(type(req_url.text)))
+    print("\t- content: {}".format(req_url.text))
