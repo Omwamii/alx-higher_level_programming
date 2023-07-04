8166bc23c2efd5e12c7f3488c451c98482c40758
omwami <ianalpha@protonmail.com>
2023-07-04 13:13:46 +0300

    Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 """ sends request to URL and displays response
 """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    res = requests.get(url)
+    if res.status_code >= 400:
+        print(f"Error code: {res.status_code}")
+    else:
+        print(res.text)
