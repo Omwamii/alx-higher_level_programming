6dc5c4916593bed385e29cf553951c5ec60bcb6d
omwami <ianalpha@protonmail.com>
2023-07-04 13:04:12 +0300

    get response header value

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -2,3 +2,13 @@
 """ script takes URL, sends request and displays value of
 X-Request-Id in response header
 """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    info = response.headers.get('X-Request-Id')
+    if info:
+        print(info)
