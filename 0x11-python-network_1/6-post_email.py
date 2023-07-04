1054c7b4a218dc4cfffc5b65b761f490afe9a340
omwami <ianalpha@protonmail.com>
2023-07-04 13:09:40 +0300

    Post email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
 """ script sends POST request with email and display response
 """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = {
+            "email": email
+            }
+    res = requests.post(url, data)
+    print(res.text)
