e6f20598cfc3632f2eee234deb8234f0649f7e86
omwami <ianalpha@protonmail.com>
2023-07-04 12:39:12 +0300

    whats my status #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 """ script that fetches https://alx-intranet.hbtn.io/status
 """
+import requests
+
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.request(url)
+    body = response.text
+    print("Body response:")
+    print(f'\t- type: {type(body)}')
+    print(f'\t- content: {body}')
