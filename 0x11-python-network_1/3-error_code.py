5227fc18f1fd76098fbfbb02f17c2f9545388f61
omwami <ianalpha@protonmail.com>
2023-07-04 12:22:27 +0300

    Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
 """ takes URL , sends request and displays reponse body
 """
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            utf8_content = response.read().decode('utf-8')
+            print(utf8_content)
+    except urllib.error.URLError as e:
+        print(f"Error code: {e.code}")
