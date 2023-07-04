63e01ce22bded88fa7523d1710c6f639d625843a
omwami <ianalpha@protonmail.com>
2023-07-04 10:25:47 +0300

    why my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ script that fetches https://alx-intranet.hbtn.io/status
 """
-from urllib.request import urlopen
+if "__name__" == "__main__":
+    from urllib.request import urlopen
 
-url = 'https://alx-intranet.hbtn.io/status'
-
-with urlopen(url) as response:
-    body = response.read()
-    utf8_content = body.decode('utf-8')
-    print('Body response:')
-    print(f'    - type: {type(body)}')
-    print(f'    - content: {body}')
-    print(f'    - utf8 content: {utf8_content}')
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urlopen(url) as response:
+        body = response.read()
+        utf8_content = body.decode('utf-8')
+        print('Body response:')
+        print(f'    - type: {type(body)}')
+        print(f'    - content: {body}')
+        print(f'    - utf8 content: {utf8_content}')
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,3 +2,15 @@
 """ script takes URL and email and sends POST request to URL
 with email as param, displays body of response header
 """
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
+import sys
+
+url = sys.argv[1]
+email = sys.argv[2]
+
+data = urlencode({'email': email}).encode('utf-8')
+request = Request(url, data=data, method='POST')
+with urlopen(request) as response:
+    body = response.read().decode('utf-8')
+    print(body)
