07d052b7e618618db1a706eccf6a6bd2e8db160c
omwami <ianalpha@protonmail.com>
2023-07-05 11:35:51 +0300

    My github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,4 +2,17 @@
 """script that takes github credentials and uses GitHub API
 to display id
 """
+import requests
+import sys
 
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    url = 'https://api.github.com/user'
+    try:
+        response = requests.get(url, auth=(user, password))
+        response.raise_for_status()
+        print(response.json().get('id'))
+    except requests.exceptions.RequestException as e:
+        print("None")
