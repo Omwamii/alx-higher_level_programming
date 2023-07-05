7f5ebae1b869cad2fd478683c0f5ee646641f1d3
omwami <ianalpha@protonmail.com>
2023-07-05 12:01:20 +0300

    Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
 """ list 10 commits from most recent to oldest of repo
 """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    url = f'https://api.github.com/repos/{owner_name}/{repo_name}/commits'
+    res = requests.get(url)
+    data = res.json()
+    count = 1
+    for item in data:
+        if count > 10:
+            break
+        print(f"{item['sha']}: {item['commit']['author'].get('name')}")
+        count += 1
