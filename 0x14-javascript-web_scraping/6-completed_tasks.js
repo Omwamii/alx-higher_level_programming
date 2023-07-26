31052d6e4a06934910709f77dc6a594dcf1e71db
omwami <ianalpha@protonmail.com>
2023-07-26 11:07:04 +0300

    completed tasks

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,13 +3,12 @@
 const request = require('request');
 const endpoint = process.argv[2];
 
-request(endpoint, function (error, response, body) {
+request.get(endpoint, { json: true }, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    const res = JSON.parse(body);
     const tasks = {};
-    for (const task of res) {
+    for (const task of body) {
       if (task.completed) {
         if (task.userId in tasks) {
           tasks[task.userId] += 1;
