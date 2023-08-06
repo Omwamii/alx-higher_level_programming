ccebd69489bfa3f3f00a2317f7f66924d77081e4
omwami <ianalpha@protonmail.com>
2023-08-06 16:06:16 +0300

    semistandard check

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
   // Fetch data from the URL using jQuery's $.ajax() method
   $.ajax({
-	  url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
-	  type: 'GET',
-	  dataType: 'json',
+    url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
+    type: 'GET',
+    dataType: 'json',
     success: function (data) {
       // Extract the hello value and display
       $('#hello').text(data.hello);
